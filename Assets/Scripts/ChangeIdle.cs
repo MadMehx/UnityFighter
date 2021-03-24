a308a500223a2168113923ae1c1e8e27f369f1e8
Robert Mohabeer <robertmohabeer@gmail.com>
2021-03-23 23:14:10 -0400

    3/23/2021 mid session push
    Added hurt idle animation

---
+++ Assets/Scripts/ChangeIdle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeIdle : MonoBehaviour
+{
+    public AnimationClip hurtIdle;
+    public AnimationClip death;
+
+    protected Animator animator;
+    protected AnimatorOverrideController animatorOverrideController;
+
+    public int healthToHurt = 40;
+    public string animationName = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        animatorOverrideController = new AnimatorOverrideController(animator.runtimeAnimatorController);
+        animator.runtimeAnimatorController = animatorOverrideController;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.GetComponent<HealthScript>().getHealth() <= healthToHurt)
+        {
+            animatorOverrideController[animationName] = hurtIdle;
+        }
+        else if (this.GetComponent<HealthScript>().getHealth() <= 0)
+        {
+            animatorOverrideController[animationName] = death;
+        }
+    }
+}
