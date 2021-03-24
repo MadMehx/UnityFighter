13bc3d248c2164cf3015b7db616f51e8edf82fb5
Robert Mohabeer <robertmohabeer@gmail.com>
2021-03-24 01:10:56 -0400

    3/24/2021
    Started throw script

---
+++ Assets/Scripts/Joe/ThrowSpecial.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowSpecial : MonoBehaviour
+{
+    public Transform throwPoint;
+    public GameObject dumbellPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int charge = 0;
+        Debug.Log(charge);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //spawn dumbell and start curl animation
+    }
+}
