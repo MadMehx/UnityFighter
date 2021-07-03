1f7ab92e882ab70a3afcbe92e6f78a0d4fef671f
MadMehx <65829543+MadMehx@users.noreply.github.com>
2021-07-03 01:26:44 -0700

    MLAPI Additions
    Network host and join buttons

---
+++ Assets/Scripts/Network/ConnectionManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAPI;
+using System;
+
+public class ConnectionManager: MonoBehaviour
+{
+    //Happened on server
+    public void Host()
+    {
+        NetworkManager.Singleton.ConnectionApprovalCallback += ApprovalCheck;
+        NetworkManager.Singleton.StartHost();
+    }
+
+    //Happens on server
+    //NOT DONE 
+    private void ApprovalCheck(byte[] arg1, ulong arg2, NetworkManager.ConnectionApprovedDelegate arg3)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Join()
+    {
+        NetworkManager.Singleton.StartClient();
+    }
+}
